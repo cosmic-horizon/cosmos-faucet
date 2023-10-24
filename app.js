31d90f35be3d03b37c83d2e9eb7aabc2bb198914
Daniel Pittman <dpdanpittman@gmail.com>
2023-10-23 21:01:43 -0600

    add checker

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /************************************/
-/* Dev by atmon3r for Bitcanna Team */ 
+/* Dev by atmon3r for Bitcanna Team */
 /* Run: node --experimental-modules --es-module-specifier-resolution=node app.js */
 /************************************/
 
@@ -28,69 +28,80 @@ const pubKeyAny = cosmos.getPubKeyAny(privKey);
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }));
 
-function sendTx(addressTo,res) {
+// Add this at the top of your code to maintain a set of used addresses.
+const usedAddresses = new Set();
+
+// Modify the sendTx function to check and mark addresses as used.
+function sendTx(addressTo, res) {
+	if (usedAddresses.has(addressTo)) {
+		res.send({ error: 'Address has already been used.' });
+		return;
+	}
+
 	cosmos.getAccounts(address).then(data => {
 		// signDoc = (1)txBody + (2)authInfo
 		// ---------------------------------- (1)txBody ----------------------------------
 		const msgSend = new message.cosmos.bank.v1beta1.MsgSend({
 			from_address: address,
 			to_address: addressTo,
-			amount: [{ denom: denom, amount: String(config.AmountSend) }]		// 7 decimal places (1000000 ubcna = 1 BCNA)
+			amount: [{ denom: denom, amount: String(config.AmountSend) }]    // 7 decimal places (1000000 ubcna = 1 BCNA)
 		});
-		
+
 		const msgSendAny = new message.google.protobuf.Any({
 			type_url: "/cosmos.bank.v1beta1.MsgSend",
 			value: message.cosmos.bank.v1beta1.MsgSend.encode(msgSend).finish()
 		});
-		
+
 		//console.log("msgSendAny: ", msgSendAny);
-		
+
 		const txBody = new message.cosmos.tx.v1beta1.TxBody({ messages: [msgSendAny], memo: config.memo });
-		
+
 		//console.log("txBody: ", txBody);
 		//return;
-		
+
 		// --------------------------------- (2)authInfo ---------------------------------
 		const signerInfo = new message.cosmos.tx.v1beta1.SignerInfo({
 			public_key: pubKeyAny,
-				mode_info: { single: { mode: message.cosmos.tx.signing.v1beta1.SignMode.SIGN_MODE_DIRECT } },
-				sequence: data.account.sequence
+			mode_info: { single: { mode: message.cosmos.tx.signing.v1beta1.SignMode.SIGN_MODE_DIRECT } },
+			sequence: data.account.sequence
 		});
-		
+
 		const feeValue = new message.cosmos.tx.v1beta1.Fee({
 			amount: [{ denom: denom, amount: String(config.feeAmount) }],
 			gas_limit: config.gasLimit
 		});
-		
+
 		const authInfo = new message.cosmos.tx.v1beta1.AuthInfo({ signer_infos: [signerInfo], fee: feeValue });
-		
+
 		// -------------------------------- sign --------------------------------
 		const signedTxBytes = cosmos.sign(txBody, authInfo, data.account.account_number, privKey);
- 
-		return cosmos.broadcast(signedTxBytes).then(
-			function(response) {res.send({response}); console.log(response) }
-		);
- 
+
+		// After successfully broadcasting the transaction, mark the address as used.
+		return cosmos.broadcast(signedTxBytes).then(function (response) {
+			usedAddresses.add(addressTo);
+			res.send({ response });
+			console.log(response);
+		});
 	});
-	
 }
 
 // Routing
 app.get('/', function (req, res) {
 	res.setHeader('Content-Type', 'application/json');
-	if (req.query.address === '') {
-		console.log(false)
-		res.send({error:'Already funded'});
+	const addressTo = req.query.address;
+
+	if (addressTo === '') {
+		console.log(false);
+		res.send({ error: 'Already funded' });
 	} else {
-		var rtnTx = sendTx(req.query.address,res)
-		
-		console.log(req.query.address)		
-	}	
-})
+		var rtnTx = sendTx(addressTo, res);
+		console.log(addressTo);
+	}
+});
 
 app.listen(config.lport, function () {
 	console.log('***********************************************')
-	console.log('* Welcome on Cosmos-faucet')	
-	console.log('* Cosmos-faucet app listening on port '+config.lport)
+	console.log('* Welcome to Cosmos-faucet')
+	console.log('* Cosmos-faucet app listening on port ' + config.lport)
 	console.log('**********************************************')
-})
+});
